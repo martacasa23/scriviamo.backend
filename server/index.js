c4a61900f1e6c754df8e720a0e55bb035b9c9b5d
Marta <martacasadei97@gmail.com>
2024-12-12 15:28:22 +0100

    connessione front

--- server/index.js
+++ server/index.js
@@ -2,29 +2,22 @@ import express from 'express';
 import multer from 'multer';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import helmet from 'helmet';
+import helmet from 'helmet'; // Importa helmet
 
-const app = express();  // Initializing the app
+const app = express();  // Inizializza l'app
 
-const PORT = process.env.PORT || 5000;  // Setting the port
+const PORT = process.env.PORT || 5000;  // Imposta la porta
 
-// Define routes and middleware here
-
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Backend server running on port ${PORT}`);
-});
-
+// Usa Helmet per aumentare la sicurezza
 app.use(helmet({
-  noCache: true, // Disables cache headers
-  contentSecurityPolicy: false, // Disable CSP
+  noCache: true, // Disabilita le intestazioni di cache
+  contentSecurityPolicy: false, // Disabilita la CSP
 }));
 
-
-
-// Usa il middleware per parsare il body
-app.use(express.json()); // Middleware per parsare JSON
+// Middleware per analizzare il corpo delle richieste
+app.use(express.json()); // Middleware per analizzare JSON
 app.use(cors({
-  origin: ['https://scriviamo.org', 'http://localhost:3000'],
+  origin: ['https://scriviamo.org', 'http://localhost:3000'], // Permetti CORS
 }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -32,10 +25,10 @@ app.use(bodyParser.json());
 // Configura Multer per gestire l'upload del file
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/');
+    cb(null, 'uploads/'); // Cartella di destinazione per i file
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname);
+    cb(null, file.originalname); // Usa il nome originale del file
   }
 });
 const upload = multer({ storage: storage });
@@ -49,7 +42,7 @@ app.post('/upload-story', upload.single('file'), (req, res) => {
       return res.status(400).json({ success: false, message: 'File non caricato' });
     }
 
-    // Verifica se tutti i campi necessari sono presenti
+    // Verifica che tutti i campi necessari siano presenti
     if (!title || !author || !plot) {
       return res.status(400).json({ success: false, message: 'Titolo, autore o trama mancanti' });
     }
@@ -62,9 +55,12 @@ app.post('/upload-story', upload.single('file'), (req, res) => {
     res.status(500).json({ success: false, message: 'Errore durante l\'upload', error });
   }
 });
+
 app.get('/api', (req, res) => {
-    res.send('Endpoint API');
+  res.send('Endpoint API');
 });
 
-
-
+// Avvia il server
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Backend server running on port ${PORT}`);
+});
